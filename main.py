b937db6267fd941231923985913b283be6fd4105
Your Name <you@example.com>
2021-10-09 14:29:58 -0500

    guesses cap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 words = ['cadena', 'capataz', 'piedra', 'cargador', 'pinza', 'arbol', 'auditorio', 'balon', 'mochila', 'trampa', 'lapiz', 'cadera', 'cabeza', 'audifonos', 'cubo', 'mueble', 'computadora', 'carro', 'camara', 'pantalla']
 
+guesses_cap = 5
+
 def main():
     pass
 
